1ffaa0b2fd651132e629d8fe77218c6ad79c8944
Ebubekir <esahin3@icloud.com>
2021-05-17 20:26:02 +0300

    ekleme

--- src/Country.java
+++ src/Country.java
@@ -3,11 +3,13 @@ public class Country {
     private String name;
     private String continent;
     private String region;
+    private String isim;
 
-    public Country(String code, String name, String continent, String region) {
+    public Country(String code, String name, String continent, String region, String isim) {
         this.code = code;
         this.name = name;
         this.continent = continent;
         this.region = region;
+        this.isim = isim;
     }
 }
