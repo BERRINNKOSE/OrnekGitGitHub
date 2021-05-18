48a22f046b09e5211cb2c38a821f378fe4dc3733
Ebubekir <esahin3@icloud.com>
2021-05-19 00:17:43 +0300

    o

--- src/Country.java
+++ src/Country.java
@@ -3,6 +3,7 @@ public class Country {
     private String name;
     private String continent;
     private String region;
+    private String isim;
 
     public Country(String code, String name, String continent, String region, String isim) {
         this.code = code;
